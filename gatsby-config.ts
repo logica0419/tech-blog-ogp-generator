aad5b8cb5e32e0cd4e2920fd6784226692ccb0d1
logica <84759675+logica0419@users.noreply.github.com>
2022-04-14 04:22:40 +0900

    enabled lint & format

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,9 +3,8 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Tech Blog OGP Generator`,
-    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-emotion"]
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-eslint"],
 };
 
 export default config;
